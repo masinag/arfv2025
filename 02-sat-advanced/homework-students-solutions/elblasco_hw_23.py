7bb6a4f432335e6c69af41ec801e1a324cebdb8d
elblasco <alessio.blascovich@studenti.unitn.it>
2025-03-18 16:47:44 +0100

    fix: yet another col_diag copied and pasted without thinking

--- 02-sat-advanced/homework-students-solutions/elblasco_hw_23.py
+++ 02-sat-advanced/homework-students-solutions/elblasco_hw_23.py
@@ -53,7 +53,7 @@ def one_queen_per_diagonal(board: dict, assertions: list, n: int):
                 col_anti_diag = (row + col) - row_diag
                 if 0 <= col_anti_diag < n and (row_diag != row or col_anti_diag != col):
                     anti_diagonal_conflicts.append(
-                        board[f"cb{row_diag}{col_diag}"]
+                        board[f"cb{row_diag}{col_anti_diag}"]
                     )
 
             assertions.append(
